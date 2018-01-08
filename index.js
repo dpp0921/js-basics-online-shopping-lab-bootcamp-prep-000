db690e65ec282c7f88f5366ae00e0f1734da8892
Chen Ye <dpp0921@gmail.com>
2018-01-08 18:31:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,20 +11,57 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ const itemPrice = Math.floor((Math.random() * 100) + 1);
+ cart.push({[item]: itemPrice});
+ console.log(item + " has been added to your cart.")
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  if(cart.length === 0){
+    return console.log("Your shopping cart is empty.")
+  } else {
+    if(cart.length === 1){
+      
+    }
+  }
 }
 
-function total() {
+function total(sum) {
   // write your code here
+  sum = 0;
+  for(var i = 0; i < cart.length; i++){
+    for(var item in cart[i]){
+    sum += cart[i][item];
+    }
+  }
+  return sum;
 }
 
 function removeFromCart(item) {
   // write your code here
+  var itemInCart = false;
+
+  for (let i = 0, l = cart.length; i < l; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      itemInCart = true;
+      cart = cart.slice(0, i).concat(cart.slice(i + 1));
+      l--;
+    }
+  }
+  if (!itemInCart) {
+    console.log("That item is not in your cart.");
+  }
+  return cart;
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(!cardNumber){
+    console.log("Sorry, we don't have a credit card on file for you.");
+    return false;
+  }
+  console.log("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".")
+  cart = [];
 }
